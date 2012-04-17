9139250ec71af520edc37517e05aec29c71657ad
group6 <group6@f7314c52-06a0-4e27-a275-9b837fc95dca>
2012-04-17 18:37:45 +0000

    fixed indentation in Grasping.java
    git-svn-id: https://svn.csail.mit.edu/rss-2012/group6/trunk@418 f7314c52-06a0-4e27-a275-9b837fc95dca

--- lab7/src/Grasping/Grasping.java
+++ lab7/src/Grasping/Grasping.java
@@ -8,30 +8,30 @@ public class Grasping {
     
     
     
-    public static void main(String[] args){
-    	Object[] valOne = new Object[] { 1.57,1200};
-    	Object[] valTwo = new Object[] { 1.57,1200};
-    	
-    	System.out.println(calculateM(valOne,valTwo));
-    }
+	public static void main(String[] args){
+		Object[] valOne = new Object[] { 1.57,1200};
+		Object[] valTwo = new Object[] { 1.57,1200};
+
+		System.out.println(calculateM(valOne,valTwo));
+	}
     
     
 
     
     
-    /**
-     * Calculate slope for a 
-     * servo.
-     * 
-     * @param readingOne
-     *            the reading one array
-     *            consisting of {theta,PWM}
-     * @param readingTwo
-     *            the reading two array
-     *            consisting of {theta,PWM}
-     * @return the slope
-     */
-    public static double calculateM(Object[] readingOne, Object[] readingTwo) {
-        return ((Double)readingTwo[0] - (Double)readingOne[0])/((Double)readingTwo[1] - (Double)readingOne[1]);
-    }
+	/**
+	 * Calculate slope for a 
+	 * servo.
+	 * 
+	 * @param readingOne
+	 *            the reading one array
+	 *            consisting of {theta,PWM}
+	 * @param readingTwo
+	 *            the reading two array
+	 *            consisting of {theta,PWM}
+	 * @return the slope
+	 */
+	public static double calculateM(Object[] readingOne, Object[] readingTwo) {
+		return ((Double)readingTwo[0] - (Double)readingOne[0])/((Double)readingTwo[1] - (Double)readingOne[1]);
+	}
 }
