152c94dfc410e577ca42815656ad7bf69a8ead13
group6 <group6@f7314c52-06a0-4e27-a275-9b837fc95dca>
2012-05-02 21:33:16 +0000

    Starting on waypoint navigation with mice
    git-svn-id: https://svn.csail.mit.edu/rss-2012/group6/trunk@453 f7314c52-06a0-4e27-a275-9b837fc95dca

---
+++ Challenge/src/Challenge/RobotController.java
@@ -0,0 +1,307 @@
+package Challenge;
+
+// imports that probably aren't needed are commented out
+
+/*
+import java.awt.Color;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+*/
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+/*
+import java.util.Scanner;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+*/
+
+
+/*
+import org.ros.message.MessageListener;
+*/
+import org.ros.message.rss_msgs.MotionMsg;
+/*
+import org.ros.message.lab5_msgs.GUIPointMsg;
+import org.ros.message.lab5_msgs.GUILineMsg;
+import org.ros.message.lab5_msgs.GUISegmentMsg;
+import org.ros.message.lab5_msgs.ColorMsg;
+*/
+import org.ros.namespace.GraphName;
+import org.ros.node.Node;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+import LocalNavigation.Mat;
+/*
+import VisualServo.VisionGUI;
+*/
+
+public class RobotController implements NodeMain, Runnable  {
+	private Node logNode;
+	
+	//State stuff
+	protected boolean firstUpdate = true;
+	public static final int IDLE=0;
+	public static final int ROTATING=1;
+	public static final int TRANSLATING=2;
+	private int state = IDLE;
+	
+	//PI Parameters
+	private double distanceError=0;
+	private double distanceErrorIntegral=0;
+	private double distanceKp=.15;
+	private double distanceKi=0.002;
+	private double distanceOutput=0;
+	private double headingDesired=0;
+	private double headingError=0;
+	private double headingErrorIntegral=0;
+	private double headingKp=.15;
+	private double headingKi=0.005;
+	private double headingOutput=0;
+	
+	// x, y, and theta record the robot's current position in the world frame
+	private double x;     // continuously updated in handleOdometry
+	private double y;     // continuously updated in handleOdometry
+	private double theta; // continuously updated in handleOdometry
+	
+	//Waypoint list
+	ArrayList<Waypoint> myWaypoints=new ArrayList<Waypoint>();
+
+	// transforms between odometry and world frames
+	private Mat odoToWorld; // initialized in handleOdometry
+	private Mat worldToOdo; // initialized in handleOdometry
+	
+	// transforms between robot and world frames
+	private Mat robotToWorld; // continuously updated in handleOdometry
+	
+	//ROS stuff to read odometry, command the motors and publish our current state
+	private Subscriber<org.ros.message.rss_msgs.OdometryMsg> odoSub;
+	
+	private Publisher<MotionMsg> motorPub;
+	private MotionMsg commandMotors;
+
+	private Publisher<org.ros.message.std_msgs.String> statePub;
+	private org.ros.message.std_msgs.String stateMsg;
+
+	private TwoMice mice = new TwoMice();
+	
+	/**
+	 * <p>
+	 * Add a waypoint to our queue of waypoints.
+	 * </p>
+	 *
+	 * @param p The Waypoint.
+	 */
+	public void addWaypoint(Waypoint p){
+		myWaypoints.add(p);
+	}
+	
+	public double fixAngle(double theta){
+		while (theta>=2*Math.PI){
+			theta-=2*Math.PI;
+		}
+		while (theta<0){
+			theta+=2*Math.PI;
+		}
+		return theta;
+	}
+	public double fixAngle2(double theta){
+		while (theta>=Math.PI){
+			theta-=2*Math.PI;
+		}
+		while (theta<-Math.PI){
+			theta+=2*Math.PI;
+		}
+		return theta;
+	}
+	/**
+	 * <p>Handle an OdometryMsg.</p>
+	 *
+	 * @param the message
+	 */
+	public void handleOdometry(org.ros.message.rss_msgs.OdometryMsg message) {
+		/* ALERT! ALERT! This doesn't actually give a flying fuck what the odometry message says.
+		 * It just uses the odometry messages as a clock to query the TwoMice object.
+		 * We could pay attention to the odometry messages if we want to, but it's not necessary.
+		 */
+		/*
+		if ( firstUpdate ) {
+			odoToWorld = Mat.mul(Mat.rotation(-message.theta), Mat.translation(-message.x, -message.y));
+			worldToOdo = Mat.inverse(odoToWorld);
+			
+			firstUpdate = false;
+		}
+
+		double[] robotPose = Mat.decodePose(Mat.mul(odoToWorld, Mat.encodePose(message.x, message.y, message.theta)));
+
+		x     = robotPose[0];
+		y     = robotPose[1];
+		theta = robotPose[2];
+		*/
+
+		mice.update();
+		x     = mice.getX();
+		y     = mice.getY();
+		theta = mice.getTheta();
+
+		theta=fixAngle(theta);
+
+		robotToWorld = Mat.mul(Mat.translation(x, y), Mat.rotation(theta));
+
+		//logNode.getLog().info("ODOM raw: " + message.x + " " + message.y + " " + message.theta +
+		//                    "\nODOM processed: " +         x + " " +         y + " " +         theta);
+		motorUpdate();
+	}
+	
+	
+	//Set of useful functions
+	public boolean comparePoints(double x1, double y1, double x2, double y2, double tolerance) {
+		return Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5) < tolerance;
+	}
+	
+	public double getAngle(double x1, double y1, double x2, double y2) {
+		return fixAngle(Math.atan2(y2-y1, x2-x1));
+	}
+	
+	public boolean compareAngles(double thetaOne, double thetaTwo, double tolerance) {
+		return Math.abs(thetaTwo-thetaOne) < tolerance;
+	}
+	
+	public double getDistance(double x1, double y1, double x2, double y2){
+		return Math.pow(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2), .5);
+	}
+	
+	
+	// Main FSM handler
+	private void motorUpdate(){
+		if (state==IDLE){
+			commandMotors.rotationalVelocity = 0;
+			commandMotors.translationalVelocity = 0;
+			if(myWaypoints.size()>0){
+				
+				//Set the desired heading, taking into account the direction we wish to face
+				if(myWaypoints.get(0).getDir()==1){
+					headingDesired=fixAngle(getAngle(x,y,myWaypoints.get(0).getX(),myWaypoints.get(0).getY()));
+				}else{
+					headingDesired=fixAngle(getAngle(myWaypoints.get(0).getX(),myWaypoints.get(0).getY(),x,y));
+				}
+				//logNode.getLog().info("Turning Towards "+myWaypoints.get(0).getX()+" , "+myWaypoints.get(0).getY());
+				changeState(ROTATING);
+			}
+		}else if(state==ROTATING){
+			headingError = fixAngle2(headingDesired - theta);
+			
+			commandMotors.translationalVelocity = 0;
+			commandMotors.rotationalVelocity = headingKp * headingError + headingKi * headingErrorIntegral;
+			headingErrorIntegral+=headingError;
+			
+			if(compareAngles(theta,headingDesired, .15)){
+				commandMotors.rotationalVelocity = 0;
+				headingErrorIntegral=0;
+				//logNode.getLog().info("Moving to "+myWaypoints.get(0).getX()+" , "+myWaypoints.get(0).getY());
+				changeState(TRANSLATING);
+			}
+
+		}else if(state==TRANSLATING){
+			distanceError=getDistance(x, y, myWaypoints.get(0).getX() ,myWaypoints.get(0).getY() );
+			
+			commandMotors.rotationalVelocity = 0;
+			commandMotors.translationalVelocity = myWaypoints.get(0).getDir()*(distanceKp * distanceError + distanceKi * distanceErrorIntegral);
+			distanceErrorIntegral += distanceError;
+			
+			if(comparePoints(x,y,myWaypoints.get(0).getX(),myWaypoints.get(0).getY(), .2 )){
+				distanceErrorIntegral=0;
+				commandMotors.translationalVelocity = 0;
+				myWaypoints.remove(0);
+				changeState(IDLE);
+			}
+		}
+	motorPub.publish(commandMotors);
+	}
+	
+	@Override
+	public void run() {
+	}
+	
+	/**
+	 * <p>
+	 * Run the RobotController process
+	 * </p>
+	 *
+	 * @param optional
+	 *   command-line argument containing hostname
+	 */
+	@Override
+	public void onStart(Node node) {
+		logNode = node;
+		logNode.getLog().info("RobotController Online");
+		// initialize the ROS subscription to rss/odometry
+		odoSub = node.newSubscriber("/rss/odometry", "rss_msgs/OdometryMsg");
+		odoSub.addMessageListener(new MessageListener<org.ros.message.rss_msgs.OdometryMsg>() {
+				@Override
+				public void onNewMessage(org.ros.message.rss_msgs.OdometryMsg message) {
+					handleOdometry(message);
+				}
+			});
+		
+		// initialize the ROS publication to command/Motors
+		motorPub = node.newPublisher("/command/Motors","rss_msgs/MotionMsg");
+		commandMotors = new MotionMsg();
+		
+		// initialize the ROS publication to rss/state
+		statePub = node.newPublisher("/rss/state","std_msgs/String");
+		stateMsg = new org.ros.message.std_msgs.String();
+		
+		Thread runningStuff = new Thread(this);
+		runningStuff.start();
+		
+/*		addWaypoint(new Waypoint(.5, 0, (short) 1));
+		addWaypoint(new Waypoint(1, 1, (short) 1));
+		addWaypoint(new Waypoint(0, 0, (short) -1));
+		addWaypoint(new Waypoint(1, 0, (short) 1));
+		addWaypoint(new Waypoint(1, 1, (short) 1));
+		addWaypoint(new Waypoint(0, 1, (short) 1));
+		addWaypoint(new Waypoint(0, 0, (short) 1));
+		addWaypoint(new Waypoint(0, 1, (short) -1));
+		addWaypoint(new Waypoint(1, 1, (short) -1));*/
+	}
+	
+	@Override
+	public void onShutdown(Node node){
+		if(node != null){
+			node.shutdown();
+		}
+	}
+
+	@Override
+	public void onShutdownComplete(Node node) {
+	}
+
+	public GraphName getDefaultNodeName() {
+		return new GraphName("rss/RobotController");
+	}
+
+	private void changeState(int newState){
+		state=newState;
+		if(state==IDLE){
+			stateMsg.data = "IDLE";
+		}else if (state==ROTATING){
+			stateMsg.data = "ROTATING";
+			System.err.println("Arrived at waypoint.");
+		}else if (state==TRANSLATING){
+			stateMsg.data = "TRANSLATING";
+		}
+		
+
+		statePub.publish(stateMsg);
+	}
+	
+	
+	
+	}
--- Challenge/src/Challenge/TwoMice.java
+++ Challenge/src/Challenge/TwoMice.java
@@ -9,33 +9,32 @@ public class TwoMice {
 	//put this in VM arguments before running
 	// -Djava.library.path=dist/
 
-	private static Controller mouseOne = null;
-	private static Controller mouseTwo = null;
+	private Controller mouseOne = null;
+	private Controller mouseTwo = null;
 
-	private static Controller keyboard = null;
+	private Controller keyboard = null;
 
-	private static final String mouseName = "DELL USB Laser Mouse";
+	private final String mouseName = "DELL USB Laser Mouse";
 
-	private static double mouseOneX = 0;
-	private static double mouseOneY = 0;
+	private double mouseOneX = 0;
+	private double mouseOneY = 0;
 
-	private static double mouseTwoX = 0;
-	private static double mouseTwoY = 0;
+	private double mouseTwoX = 0;
+	private double mouseTwoY = 0;
 
-	private static double mouseOneDeltaX = 0;
-	private static double mouseOneDeltaY = 0;
+	private double mouseOneDeltaX = 0;
+	private double mouseOneDeltaY = 0;
 
-	private static double mouseTwoDeltaX = 0;
-	private static double mouseTwoDeltaY = 0;
+	private double mouseTwoDeltaX = 0;
+	private double mouseTwoDeltaY = 0;
 
-	private static double x;
-	private static double y;
-	private static double theta;
-	private static double TICKS_PER_METER = 17000; // needs to be measured, not guessed
-	private static double DISTANCE_BETWEEN_SENSORS = .064; // meters
+	private double x;
+	private double y;
+	private double theta;
+	private double TICKS_PER_METER = 17000; // needs to be measured, not guessed
+	private double DISTANCE_BETWEEN_SENSORS = .064; // meters, needs to be measured, not guessed
 
-
-	public static void main(String args[]) {
+	public TwoMice() {
 		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
 
 		for (Controller controller : ca) {
@@ -59,94 +58,92 @@ public class TwoMice {
 		else if(keyboard == null) {
 			throw new NullPointerException("You don't have a keyboard");
 		}
+	}
 
+	public void update() {
+		mouseOne.poll();
+		Component[] componentsOne = mouseOne.getComponents();
 
-		while (true) {
-			mouseOne.poll();
-			Component[] componentsOne = mouseOne.getComponents();
-
-			for(int i=0;i< componentsOne.length;i++) {
-				if(componentsOne[i].isAnalog()) {
-					if(componentsOne[i].getName().equals("x")) {
-						mouseOneDeltaX = (double)componentsOne[i].getPollData();
-						mouseOneX += mouseOneDeltaX;
-					}
-					else if(componentsOne[i].getName().equals("y")) {
-						mouseOneDeltaY = (double)componentsOne[i].getPollData();
-						mouseOneY += mouseOneDeltaY;
-					}
+		for(int i=0;i< componentsOne.length;i++) {
+			if(componentsOne[i].isAnalog()) {
+				if(componentsOne[i].getName().equals("x")) {
+					mouseOneDeltaX = (double)componentsOne[i].getPollData();
+					mouseOneX += mouseOneDeltaX;
 				}
-			}
-
-			mouseTwo.poll();
-			Component[] componentsTwo = mouseTwo.getComponents();
-
-			for(int i=0;i< componentsTwo.length;i++) {
-				if(componentsTwo[i].isAnalog()) {
-					if(componentsTwo[i].getName().equals("x")) {
-						mouseTwoDeltaX = (double)componentsTwo[i].getPollData();
-						mouseTwoX += mouseTwoDeltaX;
-					}
-					else if(componentsTwo[i].getName().equals("y")) {
-						mouseTwoDeltaY = (double)componentsTwo[i].getPollData();
-						mouseTwoY += mouseTwoDeltaY;
-					}
+				else if(componentsOne[i].getName().equals("y")) {
+					mouseOneDeltaY = (double)componentsOne[i].getPollData();
+					mouseOneY += mouseOneDeltaY;
 				}
-			}
-
-
-			// call the update method here
-			// mouseOne is left and MouseTwo is right
-			update(getMouseOneDeltaX(),getMouseOneDeltaY(),getMouseTwoDeltaX(),getMouseTwoDeltaY());
-
-			// method to print out the values of the mouse movements :)
-			//System.out.println("Mouse X One -> " + getMouseOneX() + " Mouse Y One -> " + getMouseOneY() + " Mouse X Two -> " + getMouseTwoX() + " Mouse Y Two -> " + getMouseTwoY() );
-			System.out.println("X -> " + x + " Y -> " + y + " theta -> " + theta);
-
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
 		}
 
+		mouseTwo.poll();
+		Component[] componentsTwo = mouseTwo.getComponents();
+
+		for(int i=0;i< componentsTwo.length;i++) {
+			if(componentsTwo[i].isAnalog()) {
+				if(componentsTwo[i].getName().equals("x")) {
+					mouseTwoDeltaX = (double)componentsTwo[i].getPollData();
+					mouseTwoX += mouseTwoDeltaX;
+				}
+				else if(componentsTwo[i].getName().equals("y")) {
+					mouseTwoDeltaY = (double)componentsTwo[i].getPollData();
+					mouseTwoY += mouseTwoDeltaY;
+				}
+			}
+		}
+
+		// call the computeOdometry method here
+		// mouseOne is left and MouseTwo is right
+		computeOdometry(getMouseOneDeltaX(),getMouseOneDeltaY(),getMouseTwoDeltaX(),getMouseTwoDeltaY());
 	}
 
-
-	public static double getMouseOneX() {
+	private double getMouseOneX() {
 		return mouseOneX;
 	}
 
-	public static double getMouseOneY() {
+	private double getMouseOneY() {
 		return -mouseOneY;
 	}
 
-	public static double getMouseTwoX() {
+	private double getMouseTwoX() {
 		return mouseTwoX;
 	}
 
-	public static double getMouseTwoY() {
+	private double getMouseTwoY() {
 		return -mouseTwoY;
 	}
 
-	public static double getMouseTwoDeltaX() {
+	private double getMouseTwoDeltaX() {
 		return mouseTwoDeltaX;
 	}
 
-	public static double getMouseTwoDeltaY() {
+	private double getMouseTwoDeltaY() {
 		return -mouseTwoDeltaY;
 	}
 
-	public static double getMouseOneDeltaX() {
+	private double getMouseOneDeltaX() {
 		return mouseOneDeltaX;
 	}
 
-	public static double getMouseOneDeltaY() {
+	private double getMouseOneDeltaY() {
 		return -mouseOneDeltaY;
 	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getTheta() {
+		return theta;
+	}
+
 	// mouse 1 is left, mouse 2 is right
-	private static void update(double dx1, double dy1, double dx2, double dy2) {
+	private void computeOdometry(double dx1, double dy1, double dx2, double dy2) {
 		// ticks to meters
 		dx1 /= TICKS_PER_METER;
 		dy1 /= TICKS_PER_METER;
