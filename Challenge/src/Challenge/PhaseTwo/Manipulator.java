7b0c3711ea123f76615e2722c8f574b2d37deb1b
group6 <group6@f7314c52-06a0-4e27-a275-9b837fc95dca>
2012-05-06 21:57:35 +0000

    git-svn-id: https://svn.csail.mit.edu/rss-2012/group6/trunk@463 f7314c52-06a0-4e27-a275-9b837fc95dca

--- Challenge/src/Challenge/PhaseTwo/Manipulator.java
+++ Challenge/src/Challenge/PhaseTwo/Manipulator.java
@@ -3,6 +3,21 @@ package Challenge.PhaseTwo;
 import java.util.ArrayList;
 
 import Challenge.Waypoint;
+import Grasping.ArmMsg;
+import Grasping.BlobTracking;
+import Grasping.JointController;
+import Grasping.Node;
+import Grasping.Publisher;
+import Grasping.Subscriber;
+import Grasping.VisionGUI;
+import Grasping.Grasping.ArmListener;
+import Grasping.Grasping.videoListener;
+
+import org.ros.message.rss_msgs.ArmMsg;
+import org.ros.namespace.GraphName;
+import org.ros.node.Node;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
 
 /**
  * <p>Manipulator Controller</p>
@@ -12,22 +27,27 @@ import Challenge.Waypoint;
  **/
 
 public class Manipulator {
+
+	Publisher<ArmMsg> armPublisher;
 	
 	//Some definitions and variables
 	
 	//Constants
-	public static final double O_Z_A=5;  //Height from ground to Robot origin. To measure in m
-	public static final double A_Z_B=2;  //Height from Robot origin to Arm. To measure in m
-	public static final double A_X_B=2;  //Length from Robot origin to base of Roll Bar. To measure in m
-	public static final double L_ARM=5;  //Length of Arm. To measure in m
-	public static final double L_WRIST=1;  //Length of Wrist. to measure in m
+	public static final double I2M=.0254;  //Inches to Meters
+	public static final double O_Z_A=5*I2M;  //Height from ground to Robot origin. To measure in m
+	public static final double A_Z_B=5.25*I2M;  //Height from Robot origin to Arm. To measure in m
+	public static final double A_X_B=2.5*I2M;  //Length from Robot origin to base of Arm. To measure in m
+	public static final double L_ARM=.245;  //Length of Arm. To measure in m
+	public static final double L_WRIST=.13;  //Length of Wrist. to measure in m
+	public static final double Y_MAX=.505;  //Max Height
+	public static final double Y_MIN=.035;  //Min Height
 	
-	public static final short HAND_PORT=4;  //Servo port for gripper
+	public static final short HAND_PORT=2;  //Servo port for gripper
 	public static final int HAND_BIGOPEN=1000;  //Servo value for gripper full open
 	public static final int HAND_SMALLOPEN=800;  //Servo value for gripper open but still with IR sensor working
 	public static final int HAND_HOLD=500;  //Servo value for max gripping force
-
-	public static final short WRIST_PORT=3;  //Servo port for wrist	
+	
+	public static final short WRIST_PORT=4;  //Servo port for wrist	
 	public static final int WRIST_90=500;  //various exact angles
 	public static final int WRIST_45=825;
 	public static final int WRIST_0=1350;
@@ -36,7 +56,7 @@ public class Manipulator {
 	public static final double M_WRIST=-9.0556; //degrees to servo value slope
 	public static final int B_WRIST=1290;  //degrees to servo value y-intercept
 	
-	public static final short ARM_PORT=2;  //Servo port for arm
+	public static final short ARM_PORT=5;  //Servo port for arm
 	public static final int ARM_90=1960;  //various exact angles
 	public static final int ARM_45=1600;
 	public static final int ARM_0=1250;
@@ -49,13 +69,27 @@ public class Manipulator {
 	double w=0; //Wrist Angle
 	double a=0; //Arm Angle
 	
-	//Block list
-	ArrayList<Waypoint> myWaypoints=new ArrayList<Waypoint>();
+	
+	//Command Queue.
+	//ArrayList<Waypoint> myWaypoints=new ArrayList<Waypoint>();
+	
 	public Manipulator(){
-		goToPose(0,L_ARM+O_Z_A+A_Z_B,w);
+		armPublisher = node.newPublisher("command/Arm", "rss_msgs/ArmMsg");
+		
+		goToPose(0,L_ARM+O_Z_A+A_Z_B,w);		
 	}
 	public void servoOut(short port,int value){
-		//Implement something involving ROS
+		ArmMsg publishMsg = new ArmMsg();
+		
+		if (port==(short)5){
+			
+		}else if (port==(short)4){
+			
+		}else if (port==)
+		publishMsg.pwms = new long[] {shoulderMsg.pwms[5],WristMsg.pwms[4],GripperMsg.pwms[2],0,0,0,0,0};
+		
+		//publish the message made
+		this.armPublisher.publish(publishMsg);
 	}
 	
 	public int degToServo(double theta, double m, int b){
@@ -63,12 +97,12 @@ public class Manipulator {
 	}
 	
 	public void goToPickUp(){
-		goToY(1);
+		goToY(Y_MIN);
 		servoOut(HAND_PORT,HAND_BIGOPEN);
 	}
 	
 	public void goToY(double y){
-		a=Math.asin((y-O_Z_A-A_Z_B*Math.cos(2.5*Math.PI/180.0))/L_ARM);
+		a=Math.asin((y-O_Z_A-A_Z_B)/L_ARM);
 		w=-a;
 		servoOut(ARM_PORT,degToServo(a,M_ARM,B_ARM));
 		servoOut(WRIST_PORT,degToServo(w,M_WRIST,B_WRIST));
@@ -78,6 +112,26 @@ public class Manipulator {
 	public void goToPose(double x, double y, double t){
 		goToY(y);
 		servoOut(WRIST_PORT,degToServo(t,M_WRIST,B_WRIST));
-		goToX(x);
+	//	goToX(x);
+	}
+	
+	public void onStart(Node node) {
+		jc = new JointController(node);
+
+		gui = new VisionGUI();
+
+		blobTrack = new BlobTracking(width, height);
+
+		armSub = node.newSubscriber("rss/ArmStatus", "rss_msgs/ArmMsg");
+		armSub.addMessageListener(new ArmListener());
+
+		rssVideoSub = node.newSubscriber("rss/video", "sensor_msgs/Image");
+		rssVideoSub.addMessageListener(new videoListener());
+
+		vidPub = node.newPublisher("/rss/blobVideo", "sensor_msgs/Image");
+
+		prepareToPickUpBlock();
+		pickUpBlock();
+		putDownBlock();
 	}
 }
--- Challenge/src/Challenge/PhaseTwo/PhaseTwoController.java
+++ Challenge/src/Challenge/PhaseTwo/PhaseTwoController.java
@@ -13,25 +13,30 @@ import Challenge.Waypoint;
  **/
 
 public class PhaseTwoController {
-
+	
+	public static final int INIT=0;
+	public static final int DEPOSIT_BLOCKS=1;
+	public static final int PICK_UP=2;
+	public static final int PLACE=3;
+	public static final int DONE=4;
+	private int state;
+	
+	double x=0; //end effector x
+	double y=0; //end effector y
+	double theta=0;//end effector theta
+	
 	Structure structure;
 	Manipulator manipulator;
 	RobotController robotController;
 	ArrayList<ConstructionBlock> collectedBlocks;
-
-	double x=0; //end effector x
-	double y=0; //end effector y
-	
 	
 	public PhaseTwoController(ArrayList<ConstructionBlock> myCollectedBlocks) {
+		state=INIT;
 		collectedBlocks=myCollectedBlocks;//Pass the information about the blocks we currently have 
-				
 		structure=new Structure();
 		
 		//add ConstructionBlocks to structure to define the structure we want to make. 6 or 7 or 8 blocks total?
-		
 		manipulator=new Manipulator();
-		
 		robotController=new RobotController();
 		
 		//reset robot odometry somehow so we only have to deal with the robot's x value from this point onward. 
